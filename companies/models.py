35b58d8302128cff91065a0b2b6febc6b6409632
nythan5 <g.n.d2014@gmail.com>
2024-10-10 15:27:51 -0300

    Finalizando aula 18

---
+++ companies/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Enterprise(models.Model):
+    name = models.CharField(max_length=175)
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
